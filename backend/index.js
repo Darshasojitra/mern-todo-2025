81fd310ca6ac5e9cbde59423f561cfebafa76ae5
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-08-05 17:50:55 +0530

    worked

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,35 @@
 import e from "express";
 import { collectionName, connection } from "./dbconfig.js";
 import cors from 'cors';
-const app=e();
+const app = e();
 
 app.use(e.json());
 app.use(cors());
-app.post("/add-task", async(req,resp)=>{
+app.post("/add-task", async (req, resp) => {
     const db = await connection();
-    const collection=  await db.collection(collectionName);
+    const collection = await db.collection(collectionName);
     const result = await collection.insertOne(req.body);
-    if(result){
-        resp.send({message:'new task added',success:true,result})
-    }else{
- resp.send({message:' task not added',success:false})
+    if (result) {
+        resp.send({ message: 'new task added', success: true, result })
+    } else {
+        resp.send({ message: ' task not added', success: false })
     }
-    
+
 })
+
+app.get("/tasks", async (req, resp) => {
+    const db = await connection();
+    const collection = await db.collection(collectionName);
+    const result = await collection.find().toArray();
+    if (result) {
+        resp.send({ message: 'task list fetched', success: true, result })
+    } else {
+        resp.send({ message: 'error try after sometime', success: false })
+    }
+
+})
+
+
+
 
 app.listen(3200)
