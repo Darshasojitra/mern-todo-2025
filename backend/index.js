591de2fe759d1c8041530bac71d07415d55a1426
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-08-06 18:22:37 +0530

    worked on update form

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import e from "express";
 import { collectionName, connection } from "./dbconfig.js";
 import cors from 'cors';
+import { ObjectId } from "mongodb";
 const app = e();
 
 app.use(e.json());
@@ -23,6 +24,34 @@ app.get("/tasks", async (req, resp) => {
     const result = await collection.find().toArray();
     if (result) {
         resp.send({ message: 'task list fetched', success: true, result })
+    } else {
+        resp.send({ message: 'error try after sometime', success: false })
+    }
+
+})
+
+app.get("/task/:id", async (req, resp) => {
+    const db = await connection();
+    const collection = await db.collection(collectionName);
+     const id = req.params.id 
+    const result = await collection.findOne({_id:new ObjectId(id)});
+    if (result) {
+        resp.send({ message: 'task  fetched', success: true, result })
+    } else {
+        resp.send({ message: 'error try after sometime', success: false })
+    }
+
+})
+
+
+
+app.delete("/delete/:id", async (req, resp) => {
+    const db = await connection();
+    const id = req.params.id
+    const collection = await db.collection(collectionName);
+    const result = await collection.deleteOne({_id:new ObjectId(id)})
+    if (result) {
+        resp.send({ message: 'task deleted ', success: true, result })
     } else {
         resp.send({ message: 'error try after sometime', success: false })
     }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,9 @@
 import './style/app.css'
 import NavBar from './components/navbar'
 import { Routes,Route } from 'react-router-dom'
-import AddTask from './components/addTask'
+import AddTask from './components/AddTask'
 import List from './components/List'
+import UpdateTask from './components/UpdateTask'
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
     <Routes>
       <Route path='/' element={<List />} />
       <Route path='/add' element={<AddTask />} />
+      <Route path='/update/:id' element={<UpdateTask />} />
+
 
       </Routes>
     </>
