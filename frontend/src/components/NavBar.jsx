925e24009e69056ed96591409f21bc5d4d5e8cb5
DARSHAN SOJITRA <143096142+Darshasojitra@users.noreply.github.com>
2026-05-26 12:16:18 +0530

    Refactor NavBar component for login/logout links
    null to login & signup option

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -31,7 +31,7 @@ function NavBar() {
         <nav className='navbar'>
             <div className='logo'>To Do App</div>
             <ul className='nav-links'>
-                {
+                /* {
                     login ?
                         <>
                             <li><Link to="/">List</Link></li>
@@ -40,7 +40,21 @@ function NavBar() {
 
                         </> : null
 
-                }
+                } */
+                {
+                    login ? 
+                        <>
+                            <li><Link to="/">List</Link></li>
+                         <li><Link to="/add">Add Task</Link></li>
+                         <li><Link onClick={logout} >Logout</Link></li>
+                        </> 
+                         :  <>
+                          {/* <li><Link to="/">List</Link></li> */}
+                         <li><Link to="/login" >Login</Link></li>
+                         <li><Link to="/signup">SignUp</Link></li>
+                        </>
+
+                 }
 
 
             </ul>
