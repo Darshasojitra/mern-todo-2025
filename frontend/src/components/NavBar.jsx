8162dc7add826c9f23c3f2a2a2a65e10b009fea3
DARSHAN SOJITRA <143096142+Darshasojitra@users.noreply.github.com>
2026-05-26 14:26:43 +0530

    Update NavBar.jsx

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -49,7 +49,6 @@ function NavBar() {
                          <li><Link onClick={logout} >Logout</Link></li>
                         </> 
                          :  <>
-                          {/* <li><Link to="/">List</Link></li> */}
                          <li><Link to="/login" >Login</Link></li>
                          <li><Link to="/signup">SignUp</Link></li>
                         </>
